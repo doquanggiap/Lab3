573e594c2d92049f3a772f48e9fb60764d1da55e
doquanggiap <giapdqph34273@fpt.edu.vn>
2024-03-17 14:13:10 +0700

    🚀🚀🚀

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+const carModel = require('./carModel');
+
+const uri = 'mongodb+srv://giapdqph34273:ErPl3cD1TpWoj7aY@testmongodb.3f1oxwh.mongodb.net/giapdoquang'; // Đảm bảo thông tin chính xác
+
+// Kết nối MongoDB ở đây
+mongoose.connect(uri)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Middleware để parsing requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Route handler
+app.get('/', async (req, res) => {
+    try {
+        const cars = await carModel.find().lean();
+        console.log(cars);
+        res.send(cars);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+app.get('/add_xe', async (req, res) => {
+    await mongoose.connect(uri)
+
+    let car = {
+        ten: 'xe 30',
+        namSX: 2025,
+        hang: 'Yamama',
+        gia: 1000000
+    }
+
+    let kq = await carModel.create(car)
+    console.log(kq)
+    res.send(kq)
+})
+
+app.get('/xoa/:id', async (req, res) => {
+
+    await mongoose.connect(uri)
+
+    let id = req.params.id
+
+    console.log(id);
+
+    try {
+        await carModel.deleteOne({ _id: id })
+
+        res.redirect('../')
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server error');
+    }
+})
+
+app.get('/update/:id', async (req, res) => {
+    await mongoose.connect(uri)
+
+    let id = req.params.id
+    console.log(id);
+
+    let tenXeMoi = 'Xe Moi năm 2024 heheh'
+
+
+
+    try {
+        await carModel.updateOne({ _id: id }, { ten: tenXeMoi })
+
+        let xehois = await carModel.find({})
+        res.send(xehois)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Server error');
+    }
+})
